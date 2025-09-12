57b59b288d0c6a217b3a8dca0bff4ee263b2a351
EthieuRin <ethieu.tueny@strathmore.edu>
2025-09-12 11:04:40 +0300

    innit code

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 // Site Information
-$conf['site_name'] = "BBIT E";
+$conf['site_name'] = "BBIT Enterprise";
 $conf['site_url'] = "http://localhost/col";
 $conf['site_email'] = "admin@bbite.com";
 
@@ -13,3 +13,12 @@ $conf['db_host'] = "localhost";
 $conf['db_user'] = "root";
 $conf['db_pass'] = "";
 $conf['db_name'] = "col";
+
+// Email configuration
+$conf['mail_type'] = "smtp"; // options: smtp, sendmail, mail
+$conf['smtp_host'] = "smtp.gmail.com";
+$conf['smtp_user'] = "ethieu.tueny@strathmore.edu";
+$conf['smtp_pass'] = "sdsj tatk xynq lyxb";
+$conf['smtp_pass'] = "";
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = "ssl"; // options: ssl, tls
